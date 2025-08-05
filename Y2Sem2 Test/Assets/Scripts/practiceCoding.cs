340e173c607a92220f39a9a0785d95b526e7a753
farmerferguson <gemmaferguson691@gmail.com>
2025-08-05 11:08:34 +0200

    Added practice code
    using practice code

---
+++ Y2Sem2 Test/Assets/Scripts/practiceCoding.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class practiceCoding : MonoBehaviour
+{
+
+    int coins = 25;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        total score = 3 * (coins);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
