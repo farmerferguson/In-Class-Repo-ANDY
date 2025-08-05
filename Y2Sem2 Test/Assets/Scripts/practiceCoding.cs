c9792f6530cebc360e2e9f5f88638288daae688a
farmerferguson <gemmaferguson691@gmail.com>
2025-08-05 11:11:27 +0200

    Added cube to assign code to
    assigned code to code cube

--- Y2Sem2 Test/Assets/Scripts/practiceCoding.cs
+++ Y2Sem2 Test/Assets/Scripts/practiceCoding.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class practiceCoding : MonoBehaviour
 {
 
-    int coins = 25;
+    int coins = 3;
+    int totalScore = 25*(coins);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
-        total score = 3 * (coins);
+    print (totalScore);        
         
     }
 
